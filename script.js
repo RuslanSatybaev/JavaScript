9349833c46fe8638952cdf37902d0bba84527db7
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-04 22:38:30 +0700

    7-Lesson: Variable

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-console.log('Hello "Console"');
-console.log(777);
+var name = "Jack";
+console.log(name);
+name = 'Jane';
+console.log(name);
+var color;
+console.log(color);
+color = 'red';
+console.log(color);
+
+var mainColorl1 = 'green';
