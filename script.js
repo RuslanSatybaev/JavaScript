be91c376d2046dcd99727d97981732ade1d57e59
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-06 03:32:03 +0700

    37-Lesson: Functions Task Solutions

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-const getAge = function (birthYear, yearNow) {
-    return yearNow - birthYear;
-};
+const getAverage = (x, y, z) => (x + y + z) / 3;
 
-console.log(getAge(1988, 2023));
-
-const getAge3 = (birthYear, yearNow) => yearNow - birthYear;
-
-console.log(getAge3(1988, 2024))
-
-const canByAlcohol = (birthYear, yearNow) => {
-    const age = yearNow - birthYear;
-    return age >= 21;
-}
-console.log(canByAlcohol(1986, 2023));
+console.log(getAverage(5, 9, 10));
