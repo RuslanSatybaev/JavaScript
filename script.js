9b11e0f98c439d97c40fca2cd8c5692c5574b8fa
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-06 03:05:12 +0700

    33-Lesson: Arrow Functions

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const getAge = function (birthYear, yearNow) {
 };
 
 console.log(getAge(1988, 2023));
+
+const getAge3 = (birthYear, yearNow) => yearNow - birthYear;
+
+console.log(getAge3(1988, 2024))
+
+const canByAlcohol = (birthYear, yearNow) => {
+    const age = yearNow - birthYear;
+    return age >= 21;
+}
+console.log(canByAlcohol(1986, 2023));
