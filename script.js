a76f883c9b01485f4e6826cea34d264e99316072
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-06 20:05:03 +0700

    47-Lesson: For Loop

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-const jack = {
-    firsName: "Jack",
-    lastName: "White",
-    weight: 78,
-    height: 1.70,
-    calcBmi: function () {
-        this.bmi = this.weight / this.height ** 2;
-        return this.bmi;
-    }
+for (let i = 1; i <= 10; i++) {
+    console.log('Push ups repetition ' + i);
 }
-
-const mike = {
-    firsName: "Jack",
-    lastName: "White",
-    weight: 91,
-    height: 1.93,
-    calcBmi: function () {
-        this.bmi = this.weight / this.height ** 2;
-        return this.bmi;
-    }
-}
-
-console.log(jack.calcBmi());
-console.log(jack.bmi);
-
-console.log(mike.calcBmi());
-console.log(mike.bmi);
