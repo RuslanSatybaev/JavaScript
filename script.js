fbd7bfac5016caca1bb6ed053e1d9f32efa9290b
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-06 21:20:54 +0700

    49-50-Lesson: Loop in loop

--- script.js
+++ script.js
@@ -1,11 +1,6 @@
-const user123 = ['Ruslan', 'Satybaev', 1988, 'developer',
-    ['Zabida', 'Alfia'], true];
-
-const types = [];
-
-for (let i = 0; i < user123.length; i++) {
-    console.log(user123[i], typeof user123[i]);
-
-    types.push(typeof user123[i]);
+for (let exercise = 1; exercise <= 3; exercise++) {
+    console.log(`Exercise ${exercise}`);
+    for (let rep = 1; rep <= 5; rep++) {
+        console.log(`Rep ${rep}`)
+    }
 }
-console.log(types);
