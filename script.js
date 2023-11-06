d8150cd26e0359b63f7b5b46b7bfce7c54566c92
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-06 15:07:32 +0700

    43-Lesson: Objects Dot vs Bracket Notation

--- script.js
+++ script.js
@@ -1,16 +1,31 @@
-myData = ['Ruslan', 'Satybaev', 1974,
-    'programming', ['Zabida', 'Alfia']];
-
-console.log(myData[4]);
-
 ruslanObject = {
     name: "Ruslan",
-    lastName: "Ruslan",
-    birthYear: 50,
+    lastName: "Satybaev",
+    birthYear: 1988,
     job: "programming",
     familyMembers: [
         'Zabida', 'Alfia'
     ]
 }
 
+console.log(ruslanObject.name);
+
+console.log(ruslanObject['lastName']);
+
+const userInput = prompt('What do you want know about Ruslan?' +
+    'Choose between name, lastname, birthYear, familyMembers');
+
+if (ruslanObject[userInput]) {
+    console.log(ruslanObject[userInput])
+} else {
+    alert('Wrong input');
+}
+
+ruslanObject.location = 'Russia';
+ruslanObject['telegram'] = '@ruslansatybaev';
+
 console.log(ruslanObject);
+
+console.log(`${ruslanObject.name} has ${ruslanObject
+    .familyMembers.length} family members and the first one is ${ruslanObject
+    .familyMembers[0]}`)
