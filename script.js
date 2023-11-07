c93f4057bb6bef8a3e7bddc7d779f7fe1fcfefba
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-07 18:54:37 +0700

    Dev skills 6-Lesson: Search information

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
-let diceNumber = Math.trunc(Math.random() * 6) + 1
-console.log(diceNumber);
+'use strict'
+
+const humidities = [32, 45, 29, 19, 'error', 58, 71, 47, 33, 42, 51, 49];
+const humidities2 = [1,20,53];
+
+const calculateHumiditiesAmplitudeTwoArrays = function (h1, h2) {
+
+    const humidities = h1.concat(h2);
+    console.log(humidities);
+
+    let max = humidities[0];
+    let min = humidities[0];
+    for (let i = 1; i < humidities.length; i++) {
+        const humidity = humidities[i];
+
+        if (typeof humidity !== 'number') {
+            continue;
+        }
+        if (humidity > max) {
+            max = humidity;
+        }
+        if (humidity < min) {
+            min = humidity;
+        }
+
+    }
+    console.log(min, max);
+    return max - min;
+}
+
+console.log(calculateHumiditiesAmplitudeTwoArrays(humidities, humidities2));
