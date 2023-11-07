86659cff80378d4edec5c98d6be0e617442479e6
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-07 19:43:57 +0700

    Dev skills 8-Lesson: Debugging

--- script.js
+++ script.js
@@ -1,31 +1,17 @@
 'use strict'
 
-const humidities = [32, 45, 29, 19, 'error', 58, 71, 47, 33, 42, 51, 49];
-const humidities2 = [1,20,53];
-
-const calculateHumiditiesAmplitudeTwoArrays = function (h1, h2) {
-
-    const humidities = h1.concat(h2);
-    console.log(humidities);
-
-    let max = humidities[0];
-    let min = humidities[0];
-    for (let i = 1; i < humidities.length; i++) {
-        const humidity = humidities[i];
-
-        if (typeof humidity !== 'number') {
-            continue;
-        }
-        if (humidity > max) {
-            max = humidity;
-        }
-        if (humidity < min) {
-            min = humidity;
-        }
-
+const celciusToKelvin = function () {
+    const measurement = {
+        type: 'temp',
+        unit: 'celcius',
+        value: Number(prompt('Temperature in Celcius: '))
     }
-    console.log(min, max);
-    return max - min;
+
+    // console.table(measurement);
+    // console.warn(measurement.value);
+    // console.error(measurement.value);
+    debugger;
+    return measurement.value + 273;
 }
 
-console.log(calculateHumiditiesAmplitudeTwoArrays(humidities, humidities2));
+console.log(celciusToKelvin());
