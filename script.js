7f065414c98bd97f802bf4b57934a74e30da8b52
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-07 23:56:42 +0700

    DOM 3-Lesson: DOM manipulation

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 'use strict';
 
 console.log(document.querySelector('.guess-message').textContent);
+
+document.querySelector('.guess-message').textContent = 'Правильно';
+
+document.querySelector('.question').textContent = 7;
+
+document.querySelector('.score').textContent = 11;
+
+document.querySelector('.number-input').value = 13;
