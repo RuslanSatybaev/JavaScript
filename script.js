69f897042fac71cf632106f9d32a8d0ab6881316
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-08 03:07:21 +0700

    DOM 5-6-Lesson: Implementation logics

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
 'use strict';
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.question').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
-   const guessingNumber = Number(document.querySelector('.number-input').value);
+    const guessingNumber = Number(document.querySelector('.number-input').value);
     console.log(typeof guessingNumber);
 
     if (!guessingNumber) {
-        document.querySelector('.guess-message').textContent = 'Введите число';
+        document.querySelector('.guess-message').textContent = 'Введите число!';
+    } else if (guessingNumber === secretNumber) {
+        document.querySelector('.guess-message').textContent = 'Правильно!';
+    } else if (guessingNumber > secretNumber) {
+        if (score > 1) {
+            document.querySelector('.guess-message').textContent = 'Слишком много!';
+            score--
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.guess-message').textContent = 'Вы проиграли!';
+            document.querySelector('.score').textContent = 0;
+        }
+
+    } else if (guessingNumber < secretNumber) {
+        if (score > 1) {
+            document.querySelector('.guess-message').textContent = 'Слишком мало!';
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.guess-message').textContent = 'Вы проиграли!';
+            document.querySelector('.score').textContent = 0;
+        }
     }
+
 });
 
 
