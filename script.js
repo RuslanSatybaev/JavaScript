e4bc10056889cbf2b6bc658150eb6c359d3cc3c3
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-08 07:05:49 +0700

    DOM 10-Lesson: Refactor

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highScore = 0;
 
+const displayGuessMessage = function (message) {
+    document.querySelector('.guess-message').textContent = message;
+}
+
 document.querySelector('.check').addEventListener('click', function () {
     const guessingNumber = Number(document.querySelector('.number-input').value);
     console.log(typeof guessingNumber);
 
     if (!guessingNumber) {
-        document.querySelector('.guess-message').textContent = 'Введите число!';
+        displayGuessMessage('Введите число!');
     } else if (guessingNumber === secretNumber) {
-        document.querySelector('.guess-message').textContent = 'Правильно!';
+        displayGuessMessage('Правильно!');
         document.querySelector('.question').textContent = secretNumber;
         document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
         document.querySelector('.question').style.width = '50rem';
@@ -20,23 +24,13 @@ document.querySelector('.check').addEventListener('click', function () {
             highScore = score;
             document.querySelector('.highscore').textContent = highScore;
         }
-    } else if (guessingNumber > secretNumber) {
+    } else if (guessingNumber !== secretNumber) {
         if (score > 1) {
-            document.querySelector('.guess-message').textContent = 'Слишком много!';
+            displayGuessMessage(guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!');
             score--
             document.querySelector('.score').textContent = score;
         } else {
-            document.querySelector('.guess-message').textContent = 'Вы проиграли!';
-            document.querySelector('.score').textContent = 0;
-        }
-
-    } else if (guessingNumber < secretNumber) {
-        if (score > 1) {
-            document.querySelector('.guess-message').textContent = 'Слишком мало!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.guess-message').textContent = 'Вы проиграли!';
+            displayGuessMessage('Вы проиграли!');
             document.querySelector('.score').textContent = 0;
         }
     }
@@ -47,12 +41,8 @@ document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     document.querySelector('.question').textContent = '???';
     document.querySelector('.question').style.width = '25rem';
-    document.querySelector('.guess-message').textContent = 'Начни угадывать!';
+    displayGuessMessage('Начни угадывать!');
     document.querySelector('.score').textContent = '20';
     document.querySelector('.number-input').value = '';
     document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
-})
-
-
-
-
+});
