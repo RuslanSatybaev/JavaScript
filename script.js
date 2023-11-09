e7d39b1f46b3dc5d7b5c30220ca68ceefc58fc00
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-09 22:54:25 +0700

    DOM 11-Lesson: Modal window

--- script.js
+++ script.js
@@ -1,48 +1,17 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-let highScore = 0;
+const modalWindow = document.querySelector('.modal-window');
 
-const displayGuessMessage = function (message) {
-    document.querySelector('.guess-message').textContent = message;
+const overlay = document.querySelector('.overlay');
+
+const btnCloseModalWindow = document
+    .querySelector('.close-modal-window');
+
+const btnsShowModalWindow = document
+    .querySelectorAll('.show-modal-window');
+
+console.log(btnsShowModalWindow);
+
+for (let i = 0; i < btnsShowModalWindow.length; i++) {
+    console.log(btnsShowModalWindow[i].textContent);
 }
-
-document.querySelector('.check').addEventListener('click', function () {
-    const guessingNumber = Number(document.querySelector('.number-input').value);
-    console.log(typeof guessingNumber);
-
-    if (!guessingNumber) {
-        displayGuessMessage('Введите число!');
-    } else if (guessingNumber === secretNumber) {
-        displayGuessMessage('Правильно!');
-        document.querySelector('.question').textContent = secretNumber;
-        document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
-        document.querySelector('.question').style.width = '50rem';
-
-        if (score > highScore) {
-            highScore = score;
-            document.querySelector('.highscore').textContent = highScore;
-        }
-    } else if (guessingNumber !== secretNumber) {
-        if (score > 1) {
-            displayGuessMessage(guessingNumber > secretNumber ? 'Слишком много!' : 'Слишком мало!');
-            score--
-            document.querySelector('.score').textContent = score;
-        } else {
-            displayGuessMessage('Вы проиграли!');
-            document.querySelector('.score').textContent = 0;
-        }
-    }
-});
-
-document.querySelector('.again').addEventListener('click', function () {
-    secretNumber = Math.trunc(Math.random() * 20) + 1;
-    score = 20;
-    document.querySelector('.question').textContent = '???';
-    document.querySelector('.question').style.width = '25rem';
-    displayGuessMessage('Начни угадывать!');
-    document.querySelector('.score').textContent = '20';
-    document.querySelector('.number-input').value = '';
-    document.querySelector('body').style.backgroundColor = 'rgb(0, 0, 0)';
-});
