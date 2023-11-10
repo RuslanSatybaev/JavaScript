664795408aa2553b822d38b1bfcd5d1ec225bcd4
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-11 01:27:30 +0700

    DOM 12-Lesson: Add class

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 'use strict';
 
 const modalWindow = document.querySelector('.modal-window');
-
 const overlay = document.querySelector('.overlay');
-
 const btnCloseModalWindow = document
     .querySelector('.close-modal-window');
-
 const btnsShowModalWindow = document
     .querySelectorAll('.show-modal-window');
-
 console.log(btnsShowModalWindow);
 
-for (let i = 0; i < btnsShowModalWindow.length; i++) {
-    console.log(btnsShowModalWindow[i].textContent);
+const showModelWindow = function () {
+    console.log('Click!');
+    modalWindow.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 }
+
+const closeModalWindow = function () {
+    modalWindow.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsShowModalWindow.length; i++) {
+    btnsShowModalWindow[i].addEventListener('click',
+        showModelWindow);
+}
+
+btnCloseModalWindow.addEventListener('click',
+    closeModalWindow);
+overlay.addEventListener('click', closeModalWindow);
