250ccaba2e5ebd67ed8ab9d6dca848c473434f65
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-11 17:51:04 +0700

    DOM 14-Lesson: Game Ping

--- script.js
+++ script.js
@@ -1,38 +1,9 @@
 'use strict';
 
-const modalWindow = document.querySelector('.modal-window');
-const overlay = document.querySelector('.overlay');
-const btnCloseModalWindow = document
-    .querySelector('.close-modal-window');
-const btnsShowModalWindow = document
-    .querySelectorAll('.show-modal-window');
-console.log(btnsShowModalWindow);
+const score0Element = document.querySelector('#score--0');
+const score1Element = document.getElementById('score--1');
+const diceElement = document.querySelector('.dice');
 
-const showModelWindow = function () {
-    console.log('Click!');
-    modalWindow.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-}
-
-const closeModalWindow = function () {
-    modalWindow.classList.add('hidden');
-    overlay.classList.add('hidden');
-}
-
-for (let i = 0; i < btnsShowModalWindow.length; i++) {
-    btnsShowModalWindow[i].addEventListener('click',
-        showModelWindow);
-}
-
-btnCloseModalWindow.addEventListener('click',
-    closeModalWindow);
-overlay.addEventListener('click', closeModalWindow);
-
-document.addEventListener('keydown',
-    function (e) {
-        console.log(e.key);
-        if (e.key === 'Escape' &&
-            !modalWindow.classList.contains('hidden')) {
-            closeModalWindow();
-        }
-    })
+score0Element.textContent = 0;
+score1Element.textContent = 0;
+diceElement.classList.add('hidden');
