d65ded6af3d427c315707ccae5caf20a9be61159
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-11 19:10:32 +0700

    DOM 16-Lesson: Change player

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 let currentScore = 0;
+let activePlayer = 0;
+const totalScores = [0, 0];
 
 score0Element.textContent = 0;
 score1Element.textContent = 0;
@@ -21,8 +25,14 @@ btnRoll.addEventListener('click', function() {
 
   if (diceNumber !== 1) {
     currentScore += diceNumber;
-    current0Element.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`)
+      .textContent = currentScore;
   } else {
-
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`)
+      .textContent = currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
 });
