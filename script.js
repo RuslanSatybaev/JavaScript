dbb4f6ccbdddb17bdf197d01746cd580dbaa7787
Ruslan Satybaev <medtehnik850@gmail.com>
2023-11-11 18:44:34 +0700

    DOM 15-Lesson: Change dice

--- script.js
+++ script.js
@@ -2,8 +2,27 @@
 
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.getElementById('score--1');
+const current0Element = document.getElementById('current--0');
 const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+let currentScore = 0;
 
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
+
+btnRoll.addEventListener('click', function() {
+  const diceNumber = Math.trunc(Math.random() * 6) + 1;
+
+  diceElement.classList.remove('hidden');
+  diceElement.src = `dice${diceNumber}.png`;
+
+  if (diceNumber !== 1) {
+    currentScore += diceNumber;
+    current0Element.textContent = currentScore;
+  } else {
+
+  }
+});
